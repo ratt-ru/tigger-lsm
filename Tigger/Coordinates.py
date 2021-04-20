965e18cd59cc1dc150d427835712cde16ba6a041
Raz <razman786@gmail.com>
2021-04-20 14:43:38 +0100

    Reverted fix for new tigger version.

--- Tigger/Coordinates.py
+++ Tigger/Coordinates.py
@@ -245,8 +245,13 @@ class Projection(object):
                     raise RuntimeError("Missing RA or DEC axis")
                 ra0, dec0 = self.refsky[self.ra_axis], self.refsky[self.dec_axis]
                 self.xpix0, self.ypix0 = self.refpix[self.ra_axis], self.refpix[self.dec_axis]
-                self.xscale = self.wcs.to_header()["CDELT{}".format(self.ra_axis + 1)] * DEG
-                self.yscale = self.wcs.to_header()["CDELT{}".format(self.dec_axis + 1)] * DEG
+                refpix1 = np.array(self.refpix).copy()
+                refpix1 = self.refpix.copy()
+                refpix1[self.ra_axis] += 1
+                refpix1[self.dec_axis] += 1
+                delta = self.wcs.wcs_pix2world([refpix1], 0)[0] - self.refsky
+                self.xscale = delta[self.ra_axis] * DEG
+                self.yscale = delta[self.dec_axis] * DEG
                 has_projection = True
             except Exception as exc:
                 traceback.print_exc()
